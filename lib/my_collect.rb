e3236cfc5117a651830344575852e7f781ad2a4d
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-07 18:51:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  1st_name = []
+  array.each do |i|
+    1st_name << i 
+  end
+  1st_name
+end
 
+def my_collect(array)
+  
 
+my_collect(array) do |name|
+ name.split(" ").first 
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
