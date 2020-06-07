5f2ccc307e3fa56a4fb9fc198aaceef887704a35
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-07 18:58:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(array)
+  return_array = []
+  array.each do |e|
+    return_array << yield(e) 
+  end
+  return_array
+end
 
+my_collect(array) {|name| name.split(" ").first}
+my_collect(collection) {|lang| lang.upcase}
